dc80a9983beff83d56516f70a818418f60c8f396
XiangRu Huang <122172357+hxr303@users.noreply.github.com>
2024-01-17 00:52:17 +0100

    Update QueryExamples.R
    Signed-off-by: XiangRu Huang <122172357+hxr303@users.noreply.github.com>

--- QueryExamples.R
+++ QueryExamples.R
@@ -51,6 +51,70 @@ query5 <- 'SELECT * FROM public.msp
            ORDER BY "picture number" ASC'
 table5 <- dbGetQuery(con, query5)
 
+################# The following will be altered #################
 
+########### 3. Order data
+query <- "SELECT * FROM msp ORDER BY \"picture number\";"
+query <- "SELECT * FROM msp ORDER BY specimen ASC;"
+query <- "SELECT * FROM msp ORDER BY \"picture number\" desc;"
+query <- "SELECT * FROM msp ORDER BY specimen,\"picture number\";"
+dbExecute(con, query)
+data <- dbGetQuery(con, query)
+
+########### 4. Group data
+query <- "SELECT specimen FROM msp GROUP BY specimen;"
+query <- "SELECT specimen, count(1)
+          FROM msp GROUP BY specimen;"
+query <- "SELECT specimen, count(1),max(\"picture number\"),min(\"picture number\")
+          FROM msp GROUP BY specimen;"
+data <- dbGetQuery(con, query)
+
+########### 5. Filter
+query <- "SELECT specimen, count(1),max(\"picture number\")
+          FROM msp GROUP BY specimen
+          HAVING max(\"picture number\") < 1800;"
+
+########### 6. Conditions
+query <- "SELECT *
+          FROM msp
+          WHERE specimen = 'A. pusilus_1'
+          AND bone = 'rib';"
+query <- "SELECT *
+          FROM msp
+          WHERE specimen = 'A. pusilus_1'
+          OR bone = 'humerus';"
+query <- "SELECT *
+          FROM msp
+          WHERE specimen = 'A. pusilus_1'
+          OR bone = 'humerus'
+          AND sex = 'female';"
+query <- "SELECT *
+          FROM msp
+          WHERE \"picture number\" IN (1800, 1801, 1802);"
+query <- "SELECT *
+          FROM msp
+          WHERE bone NOT IN ('rib', 'humerus', 'scapula');"
+query <- 'SELECT *
+          FROM msp
+          WHERE "editing user" IS NOT NULL;'
+query <- "SELECT *
+          FROM msp
+          WHERE bone like '%ume%';"
+query <- "SELECT *
+          FROM msp
+          WHERE bone like 'hum%';"
+query <- "SELECT *
+          FROM msp
+          WHERE bone like '%rus';"
+query <- "SELECT *
+          FROM msp
+          WHERE bone NOT like '%rus';"
+query <- "SELECT *
+          FROM msp
+          WHERE \"picture number\" BETWEEN 1 AND 1800
+          ORDER BY \"picture number\";"
+query <- 'SELECT *
+          FROM msp
+          WHERE "editing user" IS NOT NULL;'
 
 
