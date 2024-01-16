8d343b0a150c30bb1268bd6b84dbdb12eec07267
XiangRu Huang <122172357+hxr303@users.noreply.github.com>
2024-01-17 00:53:03 +0100

    Edit database via R
    Signed-off-by: XiangRu Huang <122172357+hxr303@users.noreply.github.com>

---
+++ EditingExamples.R
@@ -0,0 +1,82 @@
+#
+# This provides database editing examples.
+#
+# Explore these to learn how you can edit the database.
+#
+
+
+
+rm(list=ls())
+
+
+
+
+#################### Software Set Up ######################
+#install.packages("RPostgreSQL")
+library(RPostgreSQL)
+
+
+###################### Connect Database ######################
+con <- dbConnect(
+  PostgreSQL(),
+  dbname = "pro306",             #name of imported database
+  port = 5432,                   #port of imported server
+  user = "postgres",             #username
+  password = "password")         #password
+
+
+###################### Examples of Query ######################
+
+########### Query the tables
+tables <- dbListTables(con)
+print(tables)
+
+########### Load the initial table
+query <- "SELECT * FROM msp;"
+data  <- dbGetQuery(con, query)
+
+########### 1. Update data
+query <- "UPDATE msp SET sex = 'male';"
+query <- "UPDATE msp
+          SET sex = 'female'
+          WHERE specimen = 'C. cristata';"
+dbExecute(con, query)
+
+########### 2. Delete data
+query <- "DELETE FROM msp"
+query <- "DELETE FROM msp
+          WHERE specimen = 'C. cristata';"
+dbExecute(con, query)
+
+########### 3. Order data
+query <- "SELECT * FROM msp ORDER BY \"picture number\";"
+query <- "SELECT * FROM msp ORDER BY specimen ASC;"
+query <- "SELECT * FROM msp ORDER BY \"picture number\" desc;"
+query <- "SELECT * FROM msp ORDER BY specimen,\"picture number\";"
+dbExecute(con, query)
+data <- dbGetQuery(con, query)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
