bc0ca0558b8e4e16407405258053919019050cd6
XiangRu Huang <122172357+hxr303@users.noreply.github.com>
2024-01-19 01:52:38 +0100

    Update EditingExamples.R
    Signed-off-by: XiangRu Huang <122172357+hxr303@users.noreply.github.com>

--- EditingExamples.R
+++ EditingExamples.R
@@ -4,15 +4,12 @@
 # Explore these to learn how you can edit the database.
 #
 
-#################### Will be further refined ######################
 
 rm(list=ls())
 
 
-
-
 #################### Software Set Up ######################
-#install.packages("RPostgreSQL")
+install.packages("RPostgreSQL")
 library(RPostgreSQL)
 
 
@@ -44,49 +41,56 @@ print(tables)
 
 ########### Load the initial table
 query <- "SELECT * FROM msp;"
-data  <- dbGetQuery(con, query)
+table  <- dbGetQuery(con, query)
 
-########### 1. Update data
+
+
+########### 1. Update data (changes to the table from the dataset)
+
+# Set all rows in "sex" column into "male" from table "msp"
 query <- "UPDATE msp SET sex = 'male';"
+dbExecute(con, query)
+
+# Set "sex" column of the "C. cristata" row in "specimen" column to "male"
 query <- "UPDATE msp
           SET sex = 'female'
           WHERE specimen = 'C. cristata';"
 dbExecute(con, query)
 
-########### 2. Delete data
+
+
+########### 2. Delete data (changes to the table from the dataset)
+
+# Delete the table "msp"
 query <- "DELETE FROM msp"
+dbExecute(con, query)
+
+# Delete the "C. cristata" row in "specimen" column from table "msp"
 query <- "DELETE FROM msp
           WHERE specimen = 'C. cristata';"
 dbExecute(con, query)
 
-########### 3. Order data
+
+
+########### 3. Order data (changes to the table from the dataset)
+
+# Get ordered table "msp" based on "picture number" in ascending order
 query <- "SELECT * FROM msp ORDER BY \"picture number\";"
+# Or
 query <- "SELECT * FROM msp ORDER BY specimen ASC;"
+dbExecute(con, query)
+
+# Get ordered table "msp" based on "picture number" in descending order
 query <- "SELECT * FROM msp ORDER BY \"picture number\" desc;"
+dbExecute(con, query)
+
+# Get ordered table "msp" based on "specimen" first, then picture number"
 query <- "SELECT * FROM msp ORDER BY specimen,\"picture number\";"
 dbExecute(con, query)
-data <- dbGetQuery(con, query)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+########### 4. Editing in R could be done by more R functions
 
 
 
