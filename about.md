861aef5d1702cd0bfddb0712deabc4eac4b49e4e
SlippyRicky <122894088+SlippyRicky@users.noreply.github.com>
2024-01-19 11:53:20 +0100

    Update Of Initial Structure for the App
    Merely implements the standard shiny desktop layout in the UI section with an adapted outlook on the project.

---
+++ about.md
@@ -0,0 +1,60 @@
+## About the Ichono-Bono Database
+
+The Ichono-Bono Database is a collection of data on bones, including their names, images, and metadata. This data can be used for a variety of purposes, including research, education, and conservation.
+
+**What is this app for?**
+
+This app allows you to search, view, and organize the data in the Ichono-Bono Database. You can also use the app to create new tables, update existing tables, insert new entries, and delete entries.
+
+**How to use this app?**
+
+The app has five main tabs:
+
+* **View Tables:** This tab allows you to view the data in the Ichono-Bono Database. You can also filter the data by a variety of criteria.
+* **Search:** This tab allows you to search for bones in the database by name, species, or other criteria.
+* **Create Tables:** This tab allows you to create new tables in the database. You can also add columns to existing tables and delete columns from existing tables.
+* **Update Tables:** This tab allows you to update existing tables in the database. You can change the values of existing entries and add new entries.
+* **Delete Tables:** This tab allows you to delete existing tables from the database.
+
+**How to get started:**
+
+1. Open the app in your web browser.
+2. Click on the "View Tables" tab to view the data in the database.
+3. Click on the "Search" tab to search for bones in the database.
+4. Use the other tabs to create, update, and delete tables in the database.
+
+**What are the benefits of using this app?**
+
+* **Easy to use:** The app is easy to use and provides a user-friendly interface for searching, viewing, and organizing bone data.
+* **Powerful features:** The app provides a variety of features for creating, updating, and deleting tables in the database.
+* **Curated data:** The data in the Ichono-Bono Database is curated by experts to ensure its accuracy and reliability.
+
+**Who should use this app?**
+
+This app is a valuable resource for anyone who is interested in bones. It can be used by researchers, educators, conservators, and anyone else who needs access to accurate and up-to-date bone data.
+
+**How to get involved:**
+
+We are always looking for ways to improve the Ichono-Bono Database. If you have any feedback or suggestions, please feel free to contact us.
+
+## How to use each tab
+
+**View Tables tab:**
+
+The "View Tables" tab allows you to view the data in the Ichono-Bono Database. You can also filter the data by a variety of criteria, such as bone name, species, and location. To view a table, simply click on its name.
+
+**Search tab:**
+
+The "Search" tab allows you to search for bones in the database. You can search by bone name, species, location, and other criteria. To search for a bone, simply enter its name or other identifying information in the search box and click the "Search" button.
+
+**Create Tables tab:**
+
+The "Create Tables" tab allows you to create new tables in the database. To create a new table, simply enter a name for the table and click the "Create Table" button. You can then add columns to the table by clicking the "Add Column" button and selecting the type of data the column should hold.
+
+**Update Tables tab:**
+
+The "Update Tables" tab allows you to update existing tables in the database. To update a table, simply click on its name and then make the desired changes. You can also add new entries to the table by clicking the "Add Entry" button and entering the data for the new entry.
+
+**Delete Tables tab:**
+
+The "Delete Tables tab" allows you to delete existing tables from the database. To delete a table, simply click on its name and then click the "Delete Table" button.
--- app.R
+++ app.R
@@ -1,51 +1,84 @@
+###############################################################################
+# # App Description:
+#   This app aims to search a database of bones of various bones with images and 
+#   metadataa attached to each entry. 
+#   The initial data was provided by Rotterdam University and compiled by the
+#   members of this project.
+# # Members:  
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# # MISC:
 #
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# ## To Be Built (TBB):
+# - App interface for the Home, search, upload, download and help menu items
+###############################################################################
 
+# Necessary Packages
 library(shiny)
+library(shinydashboard)
+library(DT)
+library(markdown)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Iniitialize Dependencies 
+about_text <- readChar("about_text.html", file.info("about_text.html")$size)
+about_page_ui <- HTML(about_text)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- dashboardPage(
+  # Header with title
+  dashboardHeader(
+    title = "Ichono-Bono Database"
+  ),
+  
+  # Sidebar with navigation tabs
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("View Tables", tabName = "view_table", icon = icon("search")),
+      menuItem("Search", tabName = "search_db", icon = icon("search")),
+      menuItem("Create Tables", tabName = "create_table", icon = icon("plus-square")),
+      menuItem("Update Tables", tabName = "update_table", icon = icon("exchange-alt")),
+      menuItem("Insert Entries", tabName = "insert_value", icon = icon("edit")),
+      menuItem("Delete Tables", tabName = "del_table", icon = icon("trash-alt")),
+      menuItem("About", tabName = "about", icon = icon("info-circle"))
     )
+  ),
+  
+  # Main body with tabs contents
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "view_table",
+              DT::renderDataTable({
+                data <- read.csv("bone_data.csv")
+                data
+              })),
+      tabItem(tabName = "search_db",
+              h2("Search Database"),
+              textInput("bone_name", label="Bone Name", value=""),
+              actionButton("search_button", "Search")),
+      tabItem(tabName = "create_table",
+              h2("Create Table")),
+      tabItem(tabName = "update_table",
+              h2("Update Table")),
+      tabItem(tabName = "insert_value",
+              h2("Insert Entry")),
+      tabItem(tabName = "del_table",
+              h2("Delete Table")),
+      ttabItem(tabName = "about",
+               h2("About"),
+               insertUI("about_page",
+                        ui = about_page_ui)
+      )
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # Sever Logic
 }
+
+# # Update the welcome message when the app starts
+# update_welcome()
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+  
+  
